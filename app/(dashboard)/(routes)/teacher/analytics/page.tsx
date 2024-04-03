8f2586dcf7985008fbe001cfc8f799c33ae50123
Sharath-wq <chandranpsharath@gmail.com>
2024-04-03 19:38:03 +0530

    analytics chart

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,27 @@
-const AnalyticsPage = () => {
-    return <div>Analytics page!</div>;
+import { getAnalytics } from '@/actions/get-analytics';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import DataCard from './_components/DataCard';
+import Chart from './_components/Chart';
+
+const AnalyticsPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect('/');
+    }
+
+    const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+    return (
+        <div className='p-6 '>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+                <DataCard value={totalSales} label='Total Sale' />
+                <DataCard value={totalRevenue} shouldFormat label='Total Revenue' />
+            </div>
+            <Chart data={data} />
+        </div>
+    );
 };
 
 export default AnalyticsPage;
