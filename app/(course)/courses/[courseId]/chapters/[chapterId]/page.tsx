6b7fc70e92fde5abbb32a061a92f09c665e91342
Sharath-wq <chandranpsharath@gmail.com>
2024-04-03 12:16:23 +0530

    course purchase ui

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,10 @@ import { Banner } from '@/components/Banner';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import VideoPlayer from './_components/VideoPlayer';
+import CourseEnrollButton from './_components/CourseEnrollButton';
+import { Separator } from '@/components/ui/separator';
+import { Preview } from '@/components/Preview';
+import { File } from 'lucide-react';
 
 const ChapterIdPage = async ({
     params,
@@ -47,6 +51,38 @@ const ChapterIdPage = async ({
                         isLocked={isLocked}
                         completeOnEnd={completeOnEnd}
                     />
+                </div>
+                <div className=''>
+                    <div className='p-4 flex flex-col md:flex-row items-center justify-between'>
+                        <h2 className='text-2xl font-semibold mb-2'>{chapter.title}</h2>
+                        {purchase ? (
+                            <div>{/* TODO: add course progress button */}</div>
+                        ) : (
+                            <CourseEnrollButton courseId={params.courseId} price={course.price!} />
+                        )}
+                    </div>
+                    <Separator />
+                    <div>
+                        <Preview value={chapter.description!} />
+                    </div>
+                    {!!attachments.length && (
+                        <>
+                            <Separator />
+                            <div className='p-4'>
+                                {attachments.map((attachment) => (
+                                    <a
+                                        className='flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline'
+                                        href={attachment.url}
+                                        key={attachment.id}
+                                        target='_blank'
+                                    >
+                                        <File />
+                                        <p className='line-clamp-1'>{attachment.name}</p>
+                                    </a>
+                                ))}
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
